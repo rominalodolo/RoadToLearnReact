8059b87a8e5e949d9ded6b747aa6afa7e1f5dd7d
rominalodolo <lodoloromina@gmail.com>
2021-11-29 14:27:32 +0200

    edit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import './index.css';
 import App from './App';
 // import Clock from './components/Clock';
 // import NumberList from './components/NumberList';
-// import Users from './components/Users';
-// import registerServiceWorker from './registerServiceWorker';
-
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
 
 // const numbers = [1, 2, 3, 4, 5];
 
